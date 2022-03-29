15e99ea1ece8475708fe19d76af9e40bc6e34437
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-03-29 14:04:20 +0100

    restaurant info card added

--- MealsToGo/src/features/restaurants/screens /restaurants.screen.js
+++ MealsToGo/src/features/restaurants/screens /restaurants.screen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { RestaurantInfo } from "../components/restaurant-info.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import {
   StatusBar,
   StyleSheet,
@@ -10,31 +10,30 @@ import {
   Platform,
 } from "react-native";
 
-export const RestaurantsScreen = ()=> {
-    return(
-        <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar/>
-        </View>
-        <View style={styles.list}>
-          <RestaurantInfo/>
-        </View>
+export const RestaurantsScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar />
+      </View>
+      <View style={styles.list}>
+        <RestaurantInfoCard />
+      </View>
     </SafeAreaView>
-    )
-    
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: StatusBar.currentHeight,
-    },
-    search: {
-      padding: 16, 
-    }, 
-    list: {
-      flex: 1, 
-      padding: 16,
-      backgroundColor: "blue",
-    }
-  });
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
