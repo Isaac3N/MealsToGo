3341e4ef792b99942fc126b31d0b2d2822bfe4ef
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-03-29 11:42:54 +0100

    temporary search solution

--- MealsToGo/App.js
+++ MealsToGo/App.js
@@ -1,4 +1,6 @@
+import React from "react"
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { Searchbar } from "react-native-paper";
 import {
   StatusBar,
   StyleSheet,
@@ -12,11 +14,11 @@ import {
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1}}>
-        <View style={{ padding: 16, backgroundColor: "green" }}>
-          <Text>Search</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar/>
         </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+        <View style={styles.list}>
           <Text>List</Text>
         </View>
       </SafeAreaView>
@@ -25,4 +27,16 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  search: {
+    padding: 16, 
+  }, 
+  list: {
+    flex: 1, 
+    padding: 16,
+    backgroundColor: "blue",
+  }
+});
