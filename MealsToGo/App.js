bf4edc3ffcdbda821e6bcffdf96db0ec47b9e8ec
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-03-29 18:55:08 +0100

    text fonts update

--- MealsToGo/App.js
+++ MealsToGo/App.js
@@ -1,10 +1,27 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { RestaurantsScreen } from "./src/features/restaurants/screens /restaurants.screen";
+
+import {
+  useFonts as UseOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
+  const [oswaldloaded] = UseOswald({
+    Oswald_400Regular,
+  });
+  const [latoloaded] = useLato({
+    Lato_400Regular,
+  });
+  if (!oswaldloaded || !latoloaded) {
+    return null;
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
