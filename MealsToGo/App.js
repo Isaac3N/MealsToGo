91a1c981f67ac02772fbdda4b70c4514f361d2ec
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-03-29 12:18:21 +0100

    restaurant info and cleaning up App.js

--- MealsToGo/App.js
+++ MealsToGo/App.js
@@ -1,42 +1,14 @@
-import React from "react"
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { Searchbar } from "react-native-paper";
-import {
-  StatusBar,
-  StyleSheet,
-  SafeAreaView,
-  Text,
-  View,
-  Platform,
-} from "react-native";
+import React from "react";
+import { RestaurantsScreen } from "./src/features/restaurants/screens /restaurants.screen";
 
-// const isAndroid = Platform.OS == "android";
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar/>
-        </View>
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
-      <StatusBar style="auto" />
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  search: {
-    padding: 16, 
-  }, 
-  list: {
-    flex: 1, 
-    padding: 16,
-    backgroundColor: "blue",
-  }
-});
+
