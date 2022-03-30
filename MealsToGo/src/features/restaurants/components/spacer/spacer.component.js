473b74c93c2702a7042e945ce736ac66675ccaaf
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-03-30 17:01:16 +0100

    bug fixes

--- MealsToGo/src/features/restaurants/components/spacer/spacer.component.js
+++ MealsToGo/src/features/restaurants/components/spacer/spacer.component.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components/native";
+import styled, { useTheme } from "styled-components/native";
 
 const sizeVariant = {
   small: 1,
@@ -18,8 +18,10 @@ const getVariant = (position, size, theme) => {
   const sizeIndex = sizeVariant[size];
   const property = positionVariant[position];
   const value = theme.space[sizeIndex];
+
   return `${property}:${value}`;
 };
+
 export const Spacer = styled.View`
   ${({ position, size, theme }) => getVariant(position, size, theme)}
 `;
