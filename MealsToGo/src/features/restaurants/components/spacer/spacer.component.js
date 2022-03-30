edcef3c0828f326bd8b4e02b835da2b28ad1332a
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-03-30 15:28:56 +0100

    updated spacer component with get variant

--- MealsToGo/src/features/restaurants/components/spacer/spacer.component.js
+++ MealsToGo/src/features/restaurants/components/spacer/spacer.component.js
@@ -1,45 +1,26 @@
 import React from "react";
 import styled from "styled-components/native";
 
-const TopSmall = styled.View`
-  margin-top: ${(props) => props.theme.space[1]};
-`;
+const sizeVariant = {
+    small : 1, 
+    medium: 2,
+    large: 3,
+}
 
-const TopMedium = styled.View`
-  margin-top: ${(props) => props.theme.space[2]};
-`;
+const positionVariant= {
+    top: "marginTop",
+    left: "marginLeft",
+    right: "marginRight",
+    bottom: "marginBottom",
+}
 
-const TopLarge = styled.View`
-  margin-top: ${(props) => props.theme.space[3]};
-`;
+const getVariant = (position, size) => `${positionVariant[position]}: ${sizeVariant[size]}
+`
+export const Spacer = styled.View `
+    ${({ position, size }) => getVariant(position, size)}
+`
 
-const LeftSmall = styled.View`
-  margin-left: ${(props) => props.theme.space[1]};
-`;
-
-const LeftMedium = styled.View`
-  margin-left: ${(props) => props.theme.space[2]};
-`;
-
-const LeftLarge = styled.View`
-  margin-left: ${(props) => props.theme.space[3]};
-`;
-
-export const Spacer = ({ variant }) => {
-  if (variant === "top.medium") {
-    return <TopMedium />;
-  }
-  if (variant === "top.large") {
-    return <TopLarge />;
-  }
-  if (variant === "left.small") {
-    return <LeftSmall />;
-  }
-  if (variant === "left.medium") {
-    return <LeftMedium />;
-  }
-  if (variant === "left.large") {
-    return <LeftLarge />;
-  }
-  return <TopSmall />;
-};
+Spacer.defaultProps = {
+    position= "top",
+    size= "small"
+}
