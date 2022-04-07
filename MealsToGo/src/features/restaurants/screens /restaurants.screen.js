e6ec6c042bd883909b97baa55d5243922705fee5
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-04-07 10:16:22 +0100

    fixed rendering of scroll wheel

--- MealsToGo/src/features/restaurants/screens /restaurants.screen.js
+++ MealsToGo/src/features/restaurants/screens /restaurants.screen.js
@@ -32,13 +32,25 @@ export const RestaurantsScreen = () => (
       <Searchbar />
     </SearchContainer>
     <FlatList
-      data={[]}
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+        { name: 7 },
+        { name: 8 },
+        { name: 9 },
+        { name: 10 },
+        { name: 11 },
+        { name: 12 },
+        { name: 13 },
+        { name: 14 },
+      ]}
       renderItem={() => <RestaurantInfoCard />}
       KeyExtractor={(item) => item.name}
       contentContainerStyle={{ padding: 16 }}
     />
-    <RestaurantListContainer>
-      <RestaurantInfoCard />
-    </RestaurantListContainer>
   </SafeArea>
 );
