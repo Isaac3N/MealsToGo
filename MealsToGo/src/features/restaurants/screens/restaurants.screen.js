dd5afaf7c9bbb9427df54b71cccb87f3d6ccc215
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-04-12 14:36:37 +0100

    updated checkpoint

--- MealsToGo/src/features/restaurants/screens/restaurants.screen.js
+++ MealsToGo/src/features/restaurants/screens/restaurants.screen.js
@@ -18,29 +18,14 @@ const RestaurantList = styled(FlatList).attrs({
 })``;
 
 export const RestaurantsScreen = () => {
-  const restaurantContext = useContext(RestaurantsScreen);
+  const restaurantContext = useContext(RestaurantsContext);
   return (
     <SafeArea>
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
       <RestaurantList
-        data={[
-          { name: 1 },
-          { name: 2 },
-          { name: 3 },
-          { name: 4 },
-          { name: 5 },
-          { name: 6 },
-          { name: 7 },
-          { name: 8 },
-          { name: 9 },
-          { name: 10 },
-          { name: 11 },
-          { name: 12 },
-          { name: 13 },
-          { name: 14 },
-        ]}
+        data={restaurantContext.restaurants}
         renderItem={() => (
           <Spacer position="bottom" size="large">
             <RestaurantInfoCard />
--- MealsToGo/src/services/restaurants/restaurants.context.js
+++ MealsToGo/src/services/restaurants/restaurants.context.js
@@ -11,7 +11,7 @@ export const RestaurantsContextProvider = ({ children }) => {
   return (
     <RestaurantsContext.Provider
       value={{
-        restaurants: [1, 2, 3, 4, 5, 6, 7],
+        restaurants: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
       }}
     >
       {children}
