f914bd5b6a1d150d3db0e114cd6b3b713075a210
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-04-12 11:24:40 +0100

    mock api updates

--- MealsToGo/src/services/restaurants/restaurants.service.js
+++ MealsToGo/src/services/restaurants/restaurants.service.js
@@ -11,15 +11,12 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   });
 };
 
-const restaurantsTransform = () => {
-  return camelize(result);
-};
-
-restaurantsRequest()
-  .then(restaurantsTransform)
-  .then((transformedResponse) => {
-    console.log(transformedResponse);
-  })
-  .catch((err) => {
-    console.log("error");
+const restaurantsTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
   });
+};
