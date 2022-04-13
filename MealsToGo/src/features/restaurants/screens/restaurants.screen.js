ab92faef5938ed1dd2980d0665e73b56b996791a
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-04-13 13:28:44 +0100

    blue activity indicator

--- MealsToGo/src/features/restaurants/screens/restaurants.screen.js
+++ MealsToGo/src/features/restaurants/screens/restaurants.screen.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
-import { FlatList, StatusBar } from "react-native";
+import { FlatList, StatusBar, View } from "react-native";
 import styled from "styled-components/native";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { ActivityIndicator, Colors } from "react-native-paper";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -20,8 +21,19 @@ const RestaurantList = styled(FlatList).attrs({
 export const RestaurantsScreen = () => {
   const restaurantContext = useContext(RestaurantsContext);
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
+
   return (
     <SafeArea>
+      {isLoading && (
+        <View style={{ position: "absolute", top: "50%", left: "50%" }}>
+          <ActivityIndicator
+            size={50}
+            style={{ marginLeft: -25 }}
+            animating={true}
+            color={Colors.blue300}
+          />
+        </View>
+      )}
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
