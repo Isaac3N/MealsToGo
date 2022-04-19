fd8aea728829cda9c0848af51dc54985f580e423
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-04-19 23:54:05 +0100

    favourites bar update

--- MealsToGo/src/components/favourites/favourites-bar.component.js
+++ MealsToGo/src/components/favourites/favourites-bar.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import { Spacer } from "../spacer/spacer.component";
 import { CompactRestaurantInfo } from "../restaurant/compact-restaurant-info.component";
 
@@ -8,14 +8,22 @@ const FavouritesWrapper = styled.View`
   padding: 10px;
 `;
 
-export const FavouritesBar = ({ favourites }) => (
+export const FavouritesBar = ({ favourites, onNavigate }) => (
   <FavouritesWrapper>
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       {favourites.map((restaurant) => {
         const key = restaurant.name;
         return (
           <Spacer key={key} position="left" size="medium">
-            <CompactRestaurantInfo restaurant={restaurant} />
+            <TouchableOpacity
+              onPress={() =>
+                onNavigate("RestaurantDetail", {
+                  restaurant,
+                })
+              }
+            >
+              <CompactRestaurantInfo restaurant={restaurant} />
+            </TouchableOpacity>
           </Spacer>
         );
       })}
