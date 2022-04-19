a2001dbb37a9cb0f06237133ad3834e89dfb5803
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-04-19 13:01:08 +0100

    updated styled view for the map component

--- MealsToGo/src/features/map/screens/map.screen.js
+++ MealsToGo/src/features/map/screens/map.screen.js
@@ -1,4 +1,13 @@
 import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
+import styled from "styled-components/native";
 
-export const MapScreen = () => <MapView />;
+const Map = styled(MapView)`
+    height: 100%
+    width: 100%`;
+
+export const MapScreen = () => (
+  <>
+    <Map />
+  </>
+);
