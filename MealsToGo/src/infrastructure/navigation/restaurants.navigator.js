4c7239e1c2d7673cf6986b07a5fc4880000cde29
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-04-19 12:08:06 +0100

    changed navigation from modal to l <-> r

--- MealsToGo/src/infrastructure/navigation/restaurants.navigator.js
+++ MealsToGo/src/infrastructure/navigation/restaurants.navigator.js
@@ -5,6 +5,7 @@ import {
   createStackNavigator,
   TransitionPresets,
 } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
 import { RestaurantDetailScreen } from "../../features/restaurants/screens/restaurant-detail.screen";
@@ -13,12 +14,7 @@ const RestaurantStack = createStackNavigator();
 
 export const RestaurantsNavigator = () => {
   return (
-    <RestaurantStack.Navigator
-      headerMode="none"
-      screenOptions={{
-        ...TransitionPresets.ModalPresentationIOS,
-      }}
-    >
+    <RestaurantStack.Navigator headerMode="none">
       <RestaurantStack.Screen
         name="Restaurants"
         component={RestaurantsScreen}
