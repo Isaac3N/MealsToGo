e4b4016e1a584c8b7c8ebccb2154c5c21c5a9f7d
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-04-20 23:47:37 +0100

    authentication flow

--- MealsToGo/App.js
+++ MealsToGo/App.js
@@ -1,6 +1,7 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
+import * as firebase from "firebase";
 
 import {
   useFonts as useOswald,
@@ -13,10 +14,38 @@ import { Navigation } from "./src/infrastructure/navigation";
 
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
-
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBYVXGVHj93IlH3oUdoeLbcJ_aX8AQBdfs",
+  authDomain: "mealstogo-a1cd0.firebaseapp.com",
+  projectId: "mealstogo-a1cd0",
+  storageBucket: "mealstogo-a1cd0.appspot.com",
+  messagingSenderId: "236000147829",
+  appId: "1:236000147829:web:e3799bd59056ad20737b3c",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword("user@test.io", "test123")
+        .then((user) => {
+          console.log(user);
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }, 2000);
+  }, []);
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -28,6 +57,8 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
+  if (!isAuthenticated) return null;
 
   return (
     <>
