40dd658117796339e1ce878854e319b4e549b400
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-04-22 00:47:14 +0100

    not authenticated workflow

--- MealsToGo/src/infrastructure/navigation/index.js
+++ MealsToGo/src/infrastructure/navigation/index.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AppNavigator } from "./app.navigator";
+import { View, Text } from "react-native";
+import { AuthenticationContext } from "../../services/authentication/authentication.context";
 
 export const Navigation = () => {
-  return <AppNavigator />;
+  const { isAuthenticated } = useContext(AuthenticationContext);
+  return isAuthenticated ? (
+    <AppNavigator />
+  ) : (
+    <View>
+      <Text> Not Authticated </Text>
+    </View>
+  );
 };
