93e37dcd2049e510f15fed8d930a743de17d53fd
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-04-22 04:54:45 +0100

    you can see your favourite screens

--- MealsToGo/src/features/restaurants/screens/restaurants.screen.js
+++ MealsToGo/src/features/restaurants/screens/restaurants.screen.js
@@ -1,32 +1,23 @@
 import React, { useContext, useState } from "react";
-import { Searchbar } from "react-native-paper";
-import {
-  FlatList,
-  Pressable,
-  StatusBar,
-  View,
-  TouchableOpacity,
-} from "react-native";
+import { FlatList, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
-import { Spacer } from "../../../components/spacer/spacer.component";
-import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
-import { SafeArea } from "../../../components/utility/safe-area.component";
-import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { ActivityIndicator, Colors } from "react-native-paper";
-import { Search } from "../components/search.component";
-import { FavouritesContext } from "../../../services/favourites/favourites.context";
+
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
 import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
 
-const RestaurantList = styled(FlatList).attrs({
-  contentContainerStyle: {
-    padding: 16,
-  },
-})``;
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { FavouritesContext } from "../../../services/favourites/favourites.context";
+
+import { Search } from "../components/search.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+import { RestaurantList } from "../components/restaurant-list.styles";
 
 const Loading = styled(ActivityIndicator)`
   margin-left: -25px;
 `;
-
 const LoadingContainer = styled.View`
   position: absolute;
   top: 50%;
@@ -34,10 +25,9 @@ const LoadingContainer = styled.View`
 `;
 
 export const RestaurantsScreen = ({ navigation }) => {
-  const restaurantContext = useContext(RestaurantsContext);
-  const { isLoading, error, restaurants } = useContext(RestaurantsContext);
-  const [isToggled, setIsToggled] = useState(false);
+  const { isLoading, restaurants } = useContext(RestaurantsContext);
   const { favourites } = useContext(FavouritesContext);
+  const [isToggled, setIsToggled] = useState(false);
 
   return (
     <SafeArea>
@@ -48,7 +38,7 @@ export const RestaurantsScreen = ({ navigation }) => {
       )}
       <Search
         isFavouritesToggled={isToggled}
-        onFavouritesToggled={() => setIsToggled(!isToggled)}
+        onFavouritesToggle={() => setIsToggled(!isToggled)}
       />
       {isToggled && (
         <FavouritesBar
